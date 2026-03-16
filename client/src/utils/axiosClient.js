af27d19b3c26b2fa6ab58412752345a0dce23d54
pratyusha-ravilla <pratyusharavilla123@gmail.com>
2026-03-16 14:02:45 +0530

    update

--- client/src/utils/axiosClient.js
+++ client/src/utils/axiosClient.js
@@ -20,10 +20,30 @@
 
 
 
+// import axios from "axios";
+
+// const axiosClient = axios.create({
+//   baseURL: import.meta.env.VITE_API_URL,
+//   withCredentials: true
+// });
+
+// axiosClient.interceptors.request.use((config) => {
+//   const user = localStorage.getItem("actirepo_user");
+//   if (user) {
+//     const token = JSON.parse(user).token;
+//     config.headers.Authorization = `Bearer ${token}`;
+//   }
+//   return config;
+// });
+
+// export default axiosClient;
+
+
+
 import axios from "axios";
 
 const axiosClient = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: "https://actify-server.onrender.com/api",
   withCredentials: true
 });
 
