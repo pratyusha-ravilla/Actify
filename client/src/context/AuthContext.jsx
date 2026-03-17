16652722ae15b9a12a1f493846a7bcdcf6a2311f
pratyusha-ravilla <pratyusharavilla123@gmail.com>
2026-03-17 12:06:40 +0530

    routes updated

--- client/src/context/AuthContext.jsx
+++ client/src/context/AuthContext.jsx
@@ -1,32 +1,32 @@
 
 //client/src/context/AuthContext.jsx
 
-import { createContext, useEffect, useState } from "react";
+// import { createContext, useEffect, useState } from "react";
 
-export const AuthContext = createContext();
+// export const AuthContext = createContext();
 
-export default function AuthProvider({ children }) {
-  const [user, setUser] = useState(null);
+// export default function AuthProvider({ children }) {
+//   const [user, setUser] = useState(null);
 
-  // Load from localStorage on page load
-  useEffect(() => {
-    const stored = localStorage.getItem("actirepo_user");
-    if (stored) setUser(JSON.parse(stored));
-  }, []);
+//   // Load from localStorage on page load
+//   useEffect(() => {
+//     const stored = localStorage.getItem("actirepo_user");
+//     if (stored) setUser(JSON.parse(stored));
+//   }, []);
 
-  const login = (data) => {
-    setUser(data);
-    localStorage.setItem("actirepo_user", JSON.stringify(data));
-  };
+//   const login = (data) => {
+//     setUser(data);
+//     localStorage.setItem("actirepo_user", JSON.stringify(data));
+//   };
 
-  const logout = () => {
-    setUser(null);
-    localStorage.removeItem("actirepo_user");
-  };
+//   const logout = () => {
+//     setUser(null);
+//     localStorage.removeItem("actirepo_user");
+//   };
 
-  return (
-    <AuthContext.Provider value={{ user, login, logout }}>
-      {children}
-    </AuthContext.Provider>
-  );
-}
+//   return (
+//     <AuthContext.Provider value={{ user, login, logout }}>
+//       {children}
+//     </AuthContext.Provider>
+//   );
+// }
--- client/src/utils/ProtectedRoute.jsx
+++ client/src/utils/ProtectedRoute.jsx
@@ -1,18 +1,35 @@
 
 //client/src/utils/ProtectedRoute.jsx
 
-import { useContext } from "react";
+// import { useContext } from "react";
+// import { Navigate } from "react-router-dom";
+// import { AuthContext } from "../context/AuthContext";
+
+// export default function ProtectedRoute({ children, roles }) {
+//   const { user } = useContext(AuthContext);
+
+//   if (!user) return <Navigate to="/login" />;
+
+//   if (roles && !roles.includes(user.role)) {
+//     return <Navigate to="/login" />;
+//   }
+
+//   return children;
+// }
+
+
+
+
 import { Navigate } from "react-router-dom";
+import { useContext } from "react";
 import { AuthContext } from "../context/AuthContext";
 
-export default function ProtectedRoute({ children, roles }) {
-  const { user } = useContext(AuthContext);
+export default function ProtectedRoute({ children }) {
+  const { user, loading } = useContext(AuthContext);
+
+  if (loading) return <div>Loading...</div>;
 
   if (!user) return <Navigate to="/login" />;
-
-  if (roles && !roles.includes(user.role)) {
-    return <Navigate to="/login" />;
-  }
 
   return children;
 }
